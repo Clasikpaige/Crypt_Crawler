dfc9ba434b26e5bd758bbc900f1ea3d7d1f0c8c5
Paige <102603571+Clasikpaige@users.noreply.github.com>
2023-05-13 20:39:49 +0800

    Update wordlist_generator.py

--- wordlist_generator.py
+++ wordlist_generator.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import string
 import random
+import argparse
 
 
 def generate_wordlist(length, count, output_file):
@@ -20,13 +21,27 @@ def hash_wordlist(hash_type, wordlist_file):
 
 
 def main():
-    length = 8
-    count = 20000
-    output_file = 'wordlist.txt'
-    hash_type = 'md5crypt'
+    parser = argparse.ArgumentParser(description='Generate a wordlist and hash it with John the Ripper.')
+    parser.add_argument('--length', type=int, default=8, help='the length of each word in the wordlist')
+    parser.add_argument('--count', type=int, default=20000, help='the number of words to generate in the wordlist')
+    parser.add_argument('--output', type=str, default='wordlist.txt', help='the name of the output file')
+    parser.add_argument('--hash-type', type=str, default='md5crypt', help='the type of hash to use with John the Ripper')
+    parser.add_argument('--target', type=str, default=None, help='the hostname or wallet to target')
+    args = parser.parse_args()
+
+    length = args.length
+    count = args.count
+    output_file = args.output
+    hash_type = args.hash_type
+    target = args.target
+
     wordlist_file = os.path.join('wordlist', output_file)
     generate_wordlist(length, count, wordlist_file)
     hash_wordlist(hash_type, wordlist_file)
+
+    if target:
+        print(f"Generating private key for target {target}...")
+        # Add code here to generate a private key for the target
 
 
 if __name__ == '__main__':
