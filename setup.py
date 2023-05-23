f9805bf72a5c2346cf594cbb5e86c936b90e952a
Paige <102603571+Clasikpaige@users.noreply.github.com>
2023-05-23 22:33:53 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ setup(
     version='1.0.0',
     packages=find_packages(),
     install_requires=[
-        'pywallet==0.3.1',
-        'cryptography==3.4.8',
-        'mnemonic==0.20',
-        'pybtc==0.3.4',
-        'pandas==1.3.0',
+        'pywallet',
+        'cryptography',
+        'mnemonic',
+        'pybtc',
+        'pandas',
     ],
     entry_points={
         'console_scripts': ['crypt-crawler=crypt_crawler:main']
